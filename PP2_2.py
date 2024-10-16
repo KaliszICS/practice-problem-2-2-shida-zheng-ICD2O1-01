375bd1a7a391e5497fccf07b502131fad466d8c2
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-10-16 13:57:46 +0000

    a

--- PP2_2.py
+++ PP2_2.py
@@ -2,17 +2,35 @@
 
 def q1(): 
   #Write Assignment code here
-
+  a = input("Input a integer:")
+  if a == 5:
+    print("The number is five")
+  else:
+    print("The number is not five")
 
 def q2(): 
   #Write Assignment code here
-
+  a = input("Input a integer:")
+  b = int(a)
+  if b > 0:
+    print("Positive")
+  else:
+    print("Negative")
 def q3(): 
   #Write Assignment code here
-
+  a = input("Input a integer:")
+  b = int(a)
+  if b % 2 == 0:
+    print("Even")
+  else:
+    print("Odd")
 def q4(): 
   #Write Assignment code here
-
+  a = input("Type'Hello'")
+  if a == "Hello":
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
 
 
 
