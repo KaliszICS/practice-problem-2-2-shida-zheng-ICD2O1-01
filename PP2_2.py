7e4d00f4b01718f11e8cc95eea4bc745ca9fc117
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-05 10:04:19 -0500

    Update PP2_2.py

--- PP2_2.py
+++ PP2_2.py
@@ -1,17 +1,32 @@
+def q1():
 
+  num1 = int(input("Input an integer: "))
+  if num1 == 5:
+      print("The number is Five")
+  else:
+      print("The number is not Five")
 
-def q1(): 
-  #Write Assignment code here
+def q2():
+  num2 = float(input("Input a number: "))
+  if num2 > 0:
+      print("Positive")
+  else:
+      print("Negative")
 
+def q3():
+  num3 = int(input("Input an integer: "))
+  if num3 % 2 == 0:
+      print("Even")
+  else:
+      print("Odd")
 
-def q2(): 
-  #Write Assignment code here
+def q4():
+  word = input('Type "Hello": ')
+  if word == "Hello":
+      print("The word is Hello")
+  else:
+      print("The word is not Hello")
 
-def q3(): 
-  #Write Assignment code here
-
-def q4(): 
-  #Write Assignment code here
 
 
 
@@ -19,7 +34,7 @@ def q4():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
+#q1()
+#q2()
+#q3()
+#q4()
